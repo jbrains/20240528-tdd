1c82fd234f952d6e2b1e831b3a2035cd0e102970
J. B. Rainsberger <me@jbrains.ca>
2024-05-15 10:50:32 -0300

    [REFACTOR] If it worked once, then it might work again.

--- lib/src/test/java/ca/jbrains/math/AddFractionsTest.java
+++ lib/src/test/java/ca/jbrains/math/AddFractionsTest.java
@@ -37,7 +37,7 @@ public class AddFractionsTest {
 
         public Fraction plus(Fraction that) {
             if (this.integerValue == 0) return new Fraction(this.integerValue + that.integerValue);
-            if (that.integerValue == 0) return this;
+            if (that.integerValue == 0) return new Fraction(this.integerValue + that.integerValue);
             return new Fraction(this.integerValue + that.integerValue);
         }
 
